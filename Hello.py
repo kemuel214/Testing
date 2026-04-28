00f5adcc4c0ee56d909f154dbf2351803118557f
kemuel214 <luke.canicula@gmail.com>
2026-04-29 07:59:15 +0800

    Updated Code

--- Hello.py
+++ Hello.py
@@ -11,7 +11,7 @@ def is_valid_name(name):
     return isinstance(name, str) and name.strip() != ""
 
 # Test cases
-user_name = "Josh"
+user_name = "Brendan"
 empty_name = ""
 none_name = None
 space_name = "   "
